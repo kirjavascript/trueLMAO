2be331d33b25dffebaa6ade7757da60596876bd8
kirjavascript <snkenjoi@gmail.com>
2021-05-17 20:13:17 +0100

    some initial controller work

--- emu/src/io.rs
+++ emu/src/io.rs
@@ -1,30 +1,51 @@
 pub struct IO {
     registers: [u8; 0x10],
-    gamepad: [u8; 2],
+    gamepad: [Gamepad; 2],
 }
+
+struct Gamepad(usize);
+
+impl From<usize> for Gamepad {
+    fn from(state: usize) -> Self {
+        Gamepad(state)
+    }
+}
+
+impl Gamepad {
+    // UDLRABCS
+
+    pub fn read(&self, select: bool) -> u8 {
+        let flip = |b: u64| (b * 0x0202020202u64 & 0x010884422010u64) % 1023;
+        0
+    }
+}
+
 
 impl IO {
     pub fn new() -> Self {
         Self {
             registers: [
-                0xA0,
-                0x7F,
-                0x7F,
-                0x7F,
+                0xA0, // version
+                0, // ctrl 1 data
+                0, // ctrl 2 data
+                0, // exp data
+                0, // ctrl 1 ctrl
+                0, // ctrl 2 ctrl
+                0, // exp control
                 0,
                 0,
                 0,
-                0xFF,
                 0,
                 0,
-                0xFF,
                 0,
                 0,
-                0xFF,
                 0,
                 0,
             ],
-            gamepad: [0, 0],
+            gamepad: [
+                0.into(),
+                0.into(),
+            ],
         }
     }
 
@@ -32,8 +53,10 @@ impl IO {
         address >>= 1;
 
         if (1u32..=2).contains(&address) {
-            // TODO: gamepad
-            0
+            let address = address as usize;
+            let ctrl = self.registers[address + 3];
+            let select = ctrl & 0x40 != 0;
+            self.gamepad[address - 1].read(select)
         } else {
             self.registers[address as usize & 0xF]
         }
