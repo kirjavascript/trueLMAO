5c0c1a1e202212e7aa7c1320a7df4c9e461429c3
kirjavascript <snkenjoi@gmail.com>
2021-06-07 21:56:45 +0100

    bad frameskipping

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -9,7 +9,6 @@ lazy_mut! {
     );
 }
 
-
 #[wasm_bindgen]
 pub fn frame() {
     unsafe { EMU.frame(true); }
@@ -33,21 +32,3 @@ pub fn screen() -> *const u8 {
 pub fn gamepad_p1(value: usize) {
     unsafe { EMU.core.mem.io.gamepad[0].set(value) }
 }
-
-// static mut CANVAS: [u8; 320 * 240 * 4] = [0; 320 * 240 * 4];
-
-// #[wasm_bindgen]
-// pub fn canvas() -> *const u8 {
-//     let screen = unsafe { EMU.gfx.screen };
-//     for (i, pixel) in screen.chunks(3).enumerate() {
-//         let index = i * 4;
-//         unsafe {
-//             CANVAS[index] = pixel[0];
-//             CANVAS[index+1] = pixel[1];
-//             CANVAS[index+2] = pixel[2];
-//             CANVAS[index+3] = 255;
-//         }
-
-//     }
-//     unsafe { CANVAS.as_ptr() }
-// }
