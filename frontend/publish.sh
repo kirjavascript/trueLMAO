08f37d66411b18db2307e364018a7bec887e5fb0
kirjavascript <snkenjoi@gmail.com>
2023-03-08 23:30:55 +0000

    add virtual rendering to VRAM

--- frontend/publish.sh
+++ frontend/publish.sh
@@ -5,11 +5,12 @@
 # windows
 CROSS_CONTAINER_ENGINE=podman
 cross build --release --target x86_64-pc-windows-gnu
+cross build --release --target i686-pc-windows-gnu
+mv ../target/x86_64-pc-windows-gnu/release/frontend_bin.exe
+cross clean
 
 # appimage
 cargo appimage
 
 # web
 sh build.sh
-
-# cross clean
